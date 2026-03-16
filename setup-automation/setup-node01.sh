50c10b96d27f7d35aed25916fbd2b0e339ec0507
Nuno Martins <nmartins@redhat.com>
2026-03-16 19:29:28 +0200

    Update setup-node01.sh

--- setup-automation/setup-node01.sh
+++ setup-automation/setup-node01.sh
@@ -3,6 +3,30 @@
 systemctl stop systemd-tmpfiles-setup.service
 systemctl disable systemd-tmpfiles-setup.service
 
+rm -rf /etc/yum.repos.d/*
+yum clean all
+subscription-manager clean
+
+retry() {
+    for i in {1..3}; do
+        echo "Attempt $i: $2"
+        if $1; then
+            return 0
+        fi
+        [ $i -lt 3 ] && sleep 5
+    done
+    echo "Failed after 3 attempts: $2"
+    exit 1
+}
+
+retry "curl -k -L https://${SATELLITE_URL}/pub/katello-server-ca.crt -o /etc/pki/ca-trust/source/anchors/${SATELLITE_URL}.ca.crt"
+retry "update-ca-trust"
+retry "rpm -Uhv --force https://${SATELLITE_URL}/pub/katello-ca-consumer-latest.noarch.rpm"
+retry "subscription-manager register --org=${SATELLITE_ORG} --activationkey=${SATELLITE_ACTIVATIONKEY}"
+retry "dnf install -y dnf-utils git nano"
+retry "dnf install -y python3-pip python3-libsemanage git ansible-core python-requests ipa-client sssd oddjob-mkhomedir postgresql-server postgresql python3-psycopg2 python3-flask	wazuh-agent"
+setenforce 0
+
 
 echo "192.168.1.10 control.zta.lab control" >> /etc/hosts
 echo "192.168.1.11 central.zta.lab  keycloak.zta.lab  opa.zta.lab" >> /etc/hosts
@@ -17,17 +41,5 @@ nmcli con mod eth1 ipv4.dns 192.168.1.11
 nmcli con mod eth1 ipv4.dns-search zta.lab
 nmcli con up eth1
 
-rm -rf /etc/yum.repos.d/*
-yum clean all
-subcription-manager clean
 
-curl -k  -L https://${SATELLITE_URL}/pub/katello-server-ca.crt -o /etc/pki/ca-trust/source/anchors/${SATELLITE_URL}.ca.crt
-update-ca-trust
-rpm -Uhv https://${SATELLITE_URL}/pub/katello-ca-consumer-latest.noarch.rpm
-subscription-manager register --org=${SATELLITE_ORG} --activationkey=${SATELLITE_ACTIVATIONKEY}
-
-##
-########
-## install python3 libraries needed for the Cloud Report
-dnf install -y python3-pip python3-libsemanage retry "dnf install -y python3-pip python3-libsemanage git ansible-core python-requests ipa-client sssd oddjob-mkhomedir postgresql-server postgresql python3-psycopg2 python3-flask	wazuh-agent"
 
