6ef903499255b42e988dcf082d00523513929e8b
Nuno Martins <nmartins@redhat.com>
2026-03-24 10:18:55 +0200

    Update setup-central.sh

--- setup-automation/setup-central.sh
+++ setup-automation/setup-central.sh
@@ -636,10 +636,10 @@ ansible-playbook -i /tmp/inventory /tmp/zta-setup.yml
 
 
 ###############################################################################
-# 12. Run integration playbook
+# 12. Run integration playbook and IDM DNS
 ###############################################################################
 ansible-playbook -i /tmp/inventory /tmp/zta-workshop-aap/integrate.yml
-
+ansible-playbook -i inventory/hosts.ini setup/configure-dns.yml 
 ###############################################################################
 # 13. IPA rewrite config (idempotent) — must run after integrate.yml
 ###############################################################################
